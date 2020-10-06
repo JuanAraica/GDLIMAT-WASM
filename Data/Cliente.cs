fc2dc236b5faf1e88bb7e830ebab3407ce110202
JUAN ARAICA NAVARRO <juan.araica.navarro@est.una.ac.cr>
2020-10-06 00:23:53 -0600

    Agregar archivos de proyecto.

---
+++ Data/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GDLIMAT_WASM.Data
+{
+    public class Cliente
+    {
+        public string NombreCliente { get; set; }
+        public string Contacto { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string Proyecto { get; set; }
+    }
+}
---
+++ Data/Respuesta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GDLIMAT_WASM.Data
+{
+    public class Respuesta
+    {
+        public int Exito { get; set; }
+        public string Mensaje { get; set; }
+        public List<Cliente> Data { get; set; }
+    }
+}
